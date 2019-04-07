1289b606208e70b419b851f971de28541f59e386
Andrew Klotz <agc.klotz@gmail.com>
2019-04-07 19:52:06 +0000

    more bitmasks

--- ptypes/reversebits_test.go
+++ ptypes/reversebits_test.go
@@ -15,6 +15,10 @@ func TestReverseBits(t *testing.T) {
 		{2, 1 << 62},
 		{4, 1 << 61},
 		{8, 1 << 60},
+		{0x00000000ffffffff, 0xffffffff00000000},
+		{0x00000000000000ff, 0xff00000000000000},
+		{0x00000000ff000000, 0x000000ff00000000},
+		{0xffffffffffffffff, 0xffffffffffffffff},
 	} {
 		if result := ptypes.ReverseBits(tt.value); result != tt.expected {
 			t.Errorf("ReverseBits(%b) = %b, expected %b", tt.value, result, tt.expected)
